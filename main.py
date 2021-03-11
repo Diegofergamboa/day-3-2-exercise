9b560677e3f96a2d46160243a56b9a8193113d1f
Diegofergamboa <diegofergamboa@hotmail.es>
2021-03-11 20:13:45 +0000

    Done try

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ weight = float(input("enter your weight in kg: "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+#Pivote variable
+bmi = weight / ( height ** 2)
+
+#Operational algorithm
+
+if bmi >= 18.5 and bmi < 25 :
+	print('normal weight')
+elif bmi >= 25 and bmi < 30 :
+	print('overweight')
+elif bmi >= 30 and bmi < 35 :
+	print('obese')
+elif bmi >= 35 :
+	print('clinical obese')
+else: 
+	print('underweight')
 
 
 
